9ef8ad300b2cc1d5ad10057f73bd40a4adf89f10
VIRUL METHDINU MEEMANA <it24103629@my.sliit.lk>
2024-08-28 09:28:48 +0530

    Update IT24103629Lab6Q2A

---
+++ Q2/IT24103629Lab6Q2A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class IT24103629Lab6Q2A
+{
+	public static void main(String[] args)
+	{
+		int count=1;
+		
+		while(count<=10)
+		{
+			System.out.print(count+" ");
+			count = count + 1;
+		}
+	}
+}
